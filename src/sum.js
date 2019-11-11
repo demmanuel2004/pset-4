a7206c031629499dc544ce730ecab03d4afaba61
Daniel Emmanuel <DEmmanuel@ucvts.tec.nj.us>
2019-11-11 13:08:44 -0500

    added files

---
+++ src/sum.js
@@ -0,0 +1,10 @@
+const readlineSync = require("readline-sync");
+
+let lowerBound = Number(readlineSync.question("Lower board"));
+let upperBound = Number(readlineSync.question("Upper bound"));
+let lowerBound = Number(readlineSync.question("Lower board"));
+let upperBound = Number(readlineSync.question("Upper board"));
+
+while (lowerBound  > upperBound){
+  lowerBound =
+}
