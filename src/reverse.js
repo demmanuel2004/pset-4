6201f0ec4bba162afe73ccff915bba865eecdcfe
Daniel Emmanuel <55405293+demmanuel2004@users.noreply.github.com>
2019-11-14 11:43:08 -0500

    Update reverse.js

--- src/reverse.js
+++ src/reverse.js
@@ -3,14 +3,30 @@ const readlineSync = require("readline-sync");
 const MIN = 1;
 const MAX = Number.MAX_SAFE_INTEGER;
 
-const POSITIVE_INTEGER = Number(readlineSync.question("\nEnter a number: "));
+const readlineSync = require("readline-sync");
+
+let reversed = "";
+let integer = 0
+
+do{
+  number = Number(readlineSync.question("Positive Integer: "));
+} while (number < Number.MIN_SAFE_INTEGER || number > Number.MAX_SAFE_INTEGER || Number.isNaN(number) || number % 1 != 0 || number <= 0)
 
 
-if
-  (POSITIVE_INTEGER !== MIN){
-    console.log("Enter a number")
-  }
 
-if (Number.isNaN(POSITIVE_INTEGER)){
-  console.log("\nInvalid.\n");
-}
+do {
+
+  integer = number % 10;
+reversed = reversed + integer + ", ";
+  number = number - integer
+number = number/10;
+
+} while (number > 10);
+
+
+
+reversed = reversed + number + "."
+
+
+
+console.log(reversed);
