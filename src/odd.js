397054221587938b0be82078816f6dc53792f543
Daniel Emmanuel <55405293+demmanuel2004@users.noreply.github.com>
2019-11-14 16:58:45 -0500

    Update odd.js

---
+++ src/odd.js
@@ -0,0 +1,9 @@
+const readlineSync = require("readline-sync");
+const NUM = Number(readlineSync.question("\nPositive integer: "));
+
+const MIN = Number.MIN_SAFE_INTEGER ;
+const MAX = Number.MAX_SAFE_INTEGER ;
+
+if ( NUM % 2 !==0) {
+	console.log("\nEven.\n");
+}
