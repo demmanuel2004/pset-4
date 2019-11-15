a13b468559b87fe714d3a05df09a9a3ef9be7d0a
Daniel Emmanuel <55405293+demmanuel2004@users.noreply.github.com>
2019-11-15 13:16:38 -0500

    Second code after Spaces

--- src/odd.js
+++ src/odd.js
@@ -23,3 +23,24 @@ num= num/10;
 num = num + endSum + "."
 
 console.log(num);}
+
+
+
+
+
+const readlineSync = require("readline-sync");
+const NUM = Number(readlineSync.question("\nPositive integer: "));
+
+
+const MIN = Number.MIN_SAFE_INTEGER ;
+const MAX = Number.MAX_SAFE_INTEGER ;
+let endSum = 0;
+
+  
+  for (let endSum = 1; endSum < NUM; endSum++) {
+    
+    if(endSum % 2 !== 0) {
+     
+      console.log(endSum);
+    }
+  }
