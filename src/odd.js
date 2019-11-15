fd0fa22e0a6375da3275f9c01d75a6ae46f071cf
Daniel Emmanuel <55405293+demmanuel2004@users.noreply.github.com>
2019-11-15 13:05:23 -0500

    Update odd.js

--- src/odd.js
+++ src/odd.js
@@ -2,15 +2,24 @@ const readlineSync = require("readline-sync");
 const NUM = Number(readlineSync.question("\nPositive integer: "));
 
 
-const MIN = Number.MIN_SAFE_INTEGER ;
-const MAX = Number.MAX_SAFE_INTEGER ;
+const MIN = Number.MIN_SAFE_INTEGER;
+const MAX = Number.MAX_SAFE_INTEGER;
+
+let num = "";
 let endSum = 0;
 
-  
-  for (let endSum = 1; endSum < NUM; endSum++) {
-    
-    if(endSum % 2 !== 0) {
-     
-      console.log(endSum);
-    }
-  }
+
+if (num % 2 !==0){
+ do {
+endSum = num % 2 !==0;
+num = num + endSum + ", ";
+num = num - endSum
+num= num/10;
+
+}
+
+ while (number > 10);
+
+num = num + endSum + "."
+
+console.log(num);}
