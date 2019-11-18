d3ba99316d806d5dab6de1e3f7afa41008c7156a
Daniel Emmanuel <55405293+demmanuel2004@users.noreply.github.com>
2019-11-17 20:27:29 -0500

    Update odd.js

--- src/odd.js
+++ src/odd.js
@@ -2,17 +2,19 @@ const readlineSync = require("readline-sync");
 
 let sum = 0;
 let value = 0;
+
 while (value <= 0) {
-value = readlineSync.question("Positive Integer: "); //345
+value = readlineSync.question("Positive Integer: "); 
 let num = value;
 while ( num > 0 )
 {
-let n = num % 10 ; // n = 5 , 4 ,3
+
+let n = num % 10 ; 
 if( n % 2 != 0)
 {
-sum = sum + n; // 5 +3
+sum = sum + n; 
 }
-num = parseInt( num / 10 ); //34 , 3 ,0
+num = parseInt( num / 10 ); 
 }
 
 }
