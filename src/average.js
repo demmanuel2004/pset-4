289371f87c4bb2874193b0ecbc7def72f1221bac
Daniel Emmanuel <55405293+demmanuel2004@users.noreply.github.com>
2019-11-17 21:23:28 -0500

    edits to average

--- src/average.js
+++ src/average.js
@@ -1,22 +1,20 @@
 const readlineSync = require("readline-sync");
 
-let count = 0; 
-let sum = 0;
+let count = 0; //how many numbers
+let sum = 0; //sum of numbers
 let value = 0;
 
-while (value <= 0) {
-value = readlineSync.question("Non=Negative integer: "); 
+while (value >= 0) {
+value = readlineSync.question("Non-Negative integer: ");
 
-    
-    num = parseInt (value);
-    
-    if (num >= 0) {
-        count++;
-        sum+= num; 
-        }
-        
-        }
-        
-        avg = sum / count;
-        avg = avg.toLocaleString(undefined, {minimumFractionDigits:3} );
-    console.log ("\n" + avg + '.');
+num = parseInt (value) ;
+
+if( num >= 0) {
+count++;
+sum += num; // sum = sum+num
+}
+}
+
+avg = sum / count;
+avg = avg.toLocaleString(undefined, {minimumFractionDigits:3} );
+console.log("\n" + avg + '.');
