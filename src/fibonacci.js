c446958dcbe52ea0cfbc101d4e008fa2b2db9fd0
Daniel Emmanuel <55405293+demmanuel2004@users.noreply.github.com>
2019-11-20 20:07:40 -0500

    Update fibonacci.js

---
+++ src/fibonacci.js
@@ -0,0 +1,27 @@
+const readlineSync = require("readline-sync");
+
+let value = 0;
+
+let a = 0;
+let b = 1;
+let c = 1;
+
+while (value < 1 || value > 78) {
+value = readlineSync.question("Non-Negative integer: ");
+
+
+num = parseInt(value);
+
+if (num > 0 && num <= 78) {
+// 0 1 2 3
+for (i = 2; i <= num; i++) { 
+c = a + b; 
+a = b;
+b = c;
+}
+}
+
+}
+
+c = c.toLocaleString();
+console.log("\n" + c + '.');
