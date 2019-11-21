df0950dc631fb482feae7c9f8bfeb19a1827bb25
Daniel Emmanuel <55405293+demmanuel2004@users.noreply.github.com>
2019-11-20 21:12:07 -0500

    edits to factors

--- src/factors.js
+++ src/factors.js
@@ -1,27 +1,24 @@
-
+const readlineSync = require("readline-sync");
 
 const MIN = Number.MIN_SAFE_INTEGER ;
 const MAX = Number.MAX_SAFE_INTEGER ;
 
-let value = -1;
+let value = 0;
 let isPrimeNumber = true;
 
-while (value <= 0 ) {
+while (value <= 0 || value > MAX) {
 value = readlineSync.question("Positive integer: ");
 
 let num = parseInt( value );
 
-for( var i = 2; i < num; i++ ) {
+for( var i = 0; i < num; i++ ) {
 if( num % i == 0) {
-isPrimeNumber = false;
-break;
-}
-}
-
-}
-
-if( isPrimeNumber) {
-console.log("\nPrime.");
-} else {
+ isPrimeNumber = false;
 console.log(i);
 }
+}
+
+}
+if( isPrimeNumber) {
+console.log("\nPrime.");
+} 
