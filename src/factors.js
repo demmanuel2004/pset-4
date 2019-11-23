e95b108f85b8d910a7155ae3c72d6e2a8e37a30c
Daniel Emmanuel <55405293+demmanuel2004@users.noreply.github.com>
2019-11-22 20:49:35 -0500

    Update factors.js

--- src/factors.js
+++ src/factors.js
@@ -1,27 +1,31 @@
 const readlineSync = require("readline-sync");
-
-const MIN = Number.MIN_SAFE_INTEGER ;
-const MAX = Number.MAX_SAFE_INTEGER ;
-
 let value = 0;
-let isPrimeNumber = true;
 
-while (value <= 0 || value > MAX) {
+while (value <= 0) {
 value = readlineSync.question("Positive integer: ");
+num = parseInt(value);
 
-let num = parseInt( value );
+limit = Math.sqrt(num);
+//81 ,limit =9
 
-for( var i = 1; i < num; i++ ) {
- if( num % i == 0) {
- isPrimeNumber = false;
-  console.log(i);
-  }
- }
+factors = '';
 
+for (var i = 1; i <= limit; i++) { //i=1,i=2,i=3 , i=4 , i=5 , i=6 , i=7 , i=8 , i=9 , i=27
+if (num % i == 0) { //81%1 =0
+smallNum = i; //smallnum=1 , 3 ,9 , 27
+largeNum = num / i; //largenum=81 27 , 9 , 3
+
+if (factors.length == 0) {
+factors = i; // factors = 1,
+} else {
+factors += ',' + smallNum;
 }
-if( isPrimeNumber) {
-console.log("\nPrime.");
-} 
 
+if (smallNum != largeNum) {
+factors += ',' + largeNum; //factors = 1,81,
+}
+}
+}
 
-
+console.log(factors);
+}
